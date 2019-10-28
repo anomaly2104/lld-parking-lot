37684b005150a97b91302dd8f5cea4c4254b0327
Udit Agarwal <udiagarw@adobe.com>
2019-10-28 20:14:04 +0530

    Add conditions for different modes of operation

--- src/main/java/com/uditagarwal/Main.java
+++ src/main/java/com/uditagarwal/Main.java
@@ -1,7 +1,25 @@
 package com.uditagarwal;
 
+import com.uditagarwal.exception.InvalidModeException;
+
 public class Main {
   public static void main(String[] args) {
     OutputPrinter.welcome();
+
+    if (isInteractiveMode(args)) {
+
+    } else if (isFileInputMode(args)) {
+
+    } else {
+      throw new InvalidModeException();
+    }
+  }
+
+  private static boolean isFileInputMode(String[] args) {
+    return args.length == 1;
+  }
+
+  private static boolean isInteractiveMode(String[] args) {
+    return args.length == 0;
   }
 }
